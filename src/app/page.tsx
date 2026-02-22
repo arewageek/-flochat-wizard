a7066254bf60b8465fa890ee97a0c5c9c8735f7d
Arewa Geek <arewageek@gmail.com>
2026-02-23 00:07:38 +0100

    feat: add Flochat component to Home page and update dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
+"use client"
+
 import { ComponentWizard } from "@/components/component-wizard";
+import { Flochat } from "@flochat/react";
 
 export default function Home() {
   return (
     <main className="app-shell antialiased">
       <ComponentWizard />
+
+      <div>
+        <Flochat
+          size="md"
+          position="bottom-left"
+          bottomOffset={32}
+          color="blue"
+          socialLinks={[
+            { platform: 'github', url: 'https://github.com/arewageek', label: 'GitHub' },
+            { platform: 'twitter', url: 'https://twitter.com/arewaofweb3', label: 'Twitter' },
+          ]}
+          showLabels={false}
+          animationStyle="grid"
+          toggleIcon="share"
+          brandColors={false}
+        />
+      </div>
     </main>
   );
 }
